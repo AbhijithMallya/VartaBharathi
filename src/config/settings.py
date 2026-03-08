b47bb5f79de07025d3343d71d1460f0d58447403
Abhijith Mallya <abhijithmallya@gmail.com>
2026-03-08 22:07:27 +0530

    - env config allow other variables

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,7 +4,7 @@ from src.config.llm import OPENAI
 from src.config.mail import GMAIL
 
 class Settings(BaseSettings):
-    model_config=SettingsConfigDict(env_file=".env", env_file_encoding='utf-8',env_nested_delimiter="__")
+    model_config=SettingsConfigDict(env_file=".env", env_file_encoding='utf-8',env_nested_delimiter="__", extra='ignore')
     openai: OPENAI
     gmail: GMAIL
 
